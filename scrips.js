523e3301f06032a41c99e40626a3761c4af0441d
Illyrian1000 <52840976+Illyrian1000@users.noreply.github.com>
2023-05-16 22:43:43 +0200

    updated var > let

--- scrips.js
+++ scrips.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var delay = 200;
-  var duration = 200;
+  let delay = 200;
+  let duration = 200;
 
-  var animatedDivs = document.querySelectorAll(".prices");
+  let animatedDivs = document.querySelectorAll(".prices");
   animatedDivs.forEach(function (div, index) {
     setTimeout(function () {
       div.style.opacity = "1";
