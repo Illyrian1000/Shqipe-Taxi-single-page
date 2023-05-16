c33d5b44fb03c017f9f5e5d5026e8f878d44e71f
Illyrian1000 <52840976+Illyrian1000@users.noreply.github.com>
2023-05-16 22:29:00 +0200

    fixed the delay and animation duration

--- scrips.js
+++ scrips.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var delay = 500; // Delay between showing each div in milliseconds
-  var duration = 500; // Duration of the animation in milliseconds
+  var delay = 200;
+  var duration = 200;
 
   var animatedDivs = document.querySelectorAll(".prices");
   animatedDivs.forEach(function (div, index) {
